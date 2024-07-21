6bba3e41ff62838b831b10a5d2dafd2fe83d2ad7
RafaSchu <94032887+RafaSchu@users.noreply.github.com>
2024-07-21 15:37:22 -0400

    Back Button added to level selection

--- app/src/main/java/com/zybooks/memorygame_prototype/HardActivity.java
+++ app/src/main/java/com/zybooks/memorygame_prototype/HardActivity.java
@@ -12,6 +12,9 @@ public class HardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hard);
 
+        Button backButton = findViewById(R.id.back_button);
+        backButton.setOnClickListener(v -> finish());
+
         Button level1Button = findViewById(R.id.level_1_button);
         level1Button.setOnClickListener(v -> startGameActivity(1));
 
