ab7eaf319e6e540877773ad47ecdfc6d39d7be1f
acortez1003 <ashtoncortez1003@gmail.com>
2024-07-21 21:59:02 -0400

    Checkbox, spinner and reset button added to Settings

--- app/src/main/java/com/zybooks/memorygame_prototype/SettingsActivity.java
+++ app/src/main/java/com/zybooks/memorygame_prototype/SettingsActivity.java
@@ -2,14 +2,13 @@ package com.zybooks.memorygame_prototype;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SettingsActivity extends AppCompatActivity {
-    private EditText userInput;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -18,23 +17,14 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
 
         Button backButton = findViewById(R.id.back_button);
-        userInput = findViewById(R.id.user_input);
-        Button validateButton = findViewById(R.id.validate_button);
+        Spinner spinner = findViewById(R.id.color_spinner);
 
         backButton.setOnClickListener(v -> finish());
 
-        validateButton.setOnClickListener(v -> validateInput());
-    }
-
-    private void validateInput() {
-        String input = userInput.getText().toString().trim();
-
-        if (input.isEmpty()) {
-            Toast.makeText(this, "Input cannot be empty", Toast.LENGTH_SHORT).show();
-        } else if (input.length() < 3) {
-            Toast.makeText(this, "Input must be at least 3 characters", Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(this, "Input is valid", Toast.LENGTH_SHORT).show();
-        }
+        // setting up spinner
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.spinner_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
